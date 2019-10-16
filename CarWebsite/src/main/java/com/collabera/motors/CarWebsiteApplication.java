e6d359afc02390cecace70e87a824b01252ab600
Dan Krueger <krugster@yahoo.com>
2019-10-16 13:36:48 -0400

    Creating backend code

---
+++ CarWebsite/src/main/java/com/collabera/motors/CarWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.collabera.motors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarWebsiteApplication.class, args);
+	}
+
+}
