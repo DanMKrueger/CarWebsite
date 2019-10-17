6445e27d99003cb7134b7880bb064d26d6ec07f6
Dan Krueger <krugster@yahoo.com>
2019-10-17 11:46:19 -0400

    Updating code

--- CarWebsite/src/main/java/com/collabera/motors/service/CarService.java
+++ CarWebsite/src/main/java/com/collabera/motors/service/CarService.java
@@ -19,4 +19,41 @@ public class CarService {
 		return (ArrayList<Car>) carRepo.findAll();
 	}
 
+	public String adminUpdate(String enteredString) {
+		
+		// Potentially parse through the entered string if neccessary
+		
+		// Get the id of the car that was sent over, and search through the database for that specific car
+		
+		// Get the whole repo, put it in an array list, and search for the car we are looking for
+		ArrayList<Car> allCars = new ArrayList<>();
+		allCars = (ArrayList<Car>) carRepo.findAll();
+		for(int i = 0; i < allCars.size(); i++) {
+			
+		}
+		
+		return "Updated";
+	}
+
+	public String adminAdd(String enteredString) {
+		
+		// Create a new Car
+		Car newCar = new Car();
+		
+		// Take that Car, and populate it with the info that was passed to us from the front end
+		
+		// Take the new Car, and save it to the database
+		// carRepo.save(newCar);
+		
+		return "Added";
+	}
+
+	public String adminRemove(String enteredString) {
+		
+		// Take the ID that was passed in, and search for it in the database to delete it.
+		//carRepo.deleteById(Integer.parseInt(enteredString));
+		
+		return "Removed";
+	}
+
 }
