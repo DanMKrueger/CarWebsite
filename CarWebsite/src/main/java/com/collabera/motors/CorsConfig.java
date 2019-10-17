dbf3fd85456952649ddc77fc33b51757caa1c2e8
Dan Krueger <krugster@yahoo.com>
2019-10-17 16:36:49 -0400

    Updated REGEX

---
+++ CarWebsite/src/main/java/com/collabera/motors/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.collabera.motors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer{
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // TODO Auto-generated method stub
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        System.out.println("This is the configuration");
+        registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowedMethods("GET","POST", "PUT", "DELETE","OPTIONS", "PATCH");
+        
+    }
+}
