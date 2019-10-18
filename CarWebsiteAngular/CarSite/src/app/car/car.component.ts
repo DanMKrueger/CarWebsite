363cfd7927d8b5c20eeefb21d82d73c505d5fe6e
Abdulbasit Raza <Abdul@Abdulbasits-MacBook-Pro.local>
2019-10-18 10:06:51 -0400

    dan

--- CarWebsiteAngular/CarSite/src/app/car/car.component.ts
+++ CarWebsiteAngular/CarSite/src/app/car/car.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgModule } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.css']
 })
+
+
+
 export class CarComponent implements OnInit {
 
-  constructor() { }
+  response: any;
+
+  constructor(
+    private carHttp: HttpClient
+  ) { }
 
   ngOnInit() {
+    this.carHttp.get('http://localhost:8080/cars').subscribe((response) => {
+      this.response = response;
+      console.log(this.response);
+    });
+
   }
+
 
 }
