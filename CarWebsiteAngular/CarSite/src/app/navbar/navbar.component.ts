66b79571821984de9b5a6afc19767e43316e14e5
Dan Krueger <krugster@yahoo.com>
2019-10-24 15:58:51 -0400

    Updated Code

--- CarWebsiteAngular/CarSite/src/app/navbar/navbar.component.ts
+++ CarWebsiteAngular/CarSite/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/compiler/src/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  signupClicked(){
+    this.router.navigateByUrl('/signup');
   }
 
 }
