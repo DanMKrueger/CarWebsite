e533c5924569f766f0abc86f226edca5537ffa70
Dan Krueger <krugster@yahoo.com>
2019-10-24 09:42:25 -0400

    Updated Code

--- CarWebsite/src/main/java/com/collabera/motors/service/ManufacturerService.java
+++ CarWebsite/src/main/java/com/collabera/motors/service/ManufacturerService.java
@@ -20,15 +20,21 @@ public class ManufacturerService {
 	private CarRepository carRepo;
 
 	public ArrayList<Manufacturer> getAllManufacturers() {
-		
+		// get all the manufacturers and return that in an array list form.
 		return (ArrayList<Manufacturer>) manRepo.findAll();
 		
 	}
 
 	public ArrayList<Car> getCars(String name) {
+		// The passed in string called name is the manufacturer name, like dodge, nissan, etc.
+		// Create an array list, and get every car from our data base.
 		ArrayList<Car> allCars = (ArrayList<Car>) carRepo.findAll();
 		ArrayList<Car> requestedCars = new ArrayList<>();
 		
+		// Now that we have all our cars, we can loop through our cars and check the make of all of them.
+		// If the make is equal to the make they requested (name), then we know that is one of the cars they want
+		// to see, so we add that car to our empty array list named requestedCars. At the end, we return that array list
+		// with all the cars with the requested make.
 		for(int i = 0; i < allCars.size(); i++) {
 			if(allCars.get(i).getMake().equals(name)) {
 				requestedCars.add(allCars.get(i));
